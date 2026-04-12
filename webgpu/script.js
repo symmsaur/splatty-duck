d361f31a479076cf93204be3f84263a665a812f7
Jonathan Karlsson <karlsjona@gmail.com>
2026-04-12 20:20:29 +0200

    Extract vertex shader to own file

--- webgpu/script.js
+++ webgpu/script.js
@@ -125,40 +125,6 @@ function rotZMat(phi) {
   return M;
 }
 
-
-const splatVertexWgsl = `
-
-
-
-struct VertexInput {
-    @builtin(instance_index) instance_index: u32,
-    @location(0) position: vec2f,
-    @location(1) color: vec3f,
-    @location(2) opacity: f32,
-    @location(3) quad_pos: vec2f,
-    @location(4) eigen: vec4f,
-}
-
-struct VertexOutput {
-    @builtin(position) position : vec4f,
-    @location(0) color : vec4f,
-    @location(1) uv: vec2f,
-}
-
-
-@vertex
-fn vs_main(in : VertexInput) -> VertexOutput {
-    // var position = vec3f(in.position + 0.1 * in.quad_pos, 1.0);
-    var eigen_mat = 3 * mat2x2f(in.eigen.xy, in.eigen.zw);
-    var position = in.position + eigen_mat * in.quad_pos;
-
-    var out : VertexOutput;
-    out.position = vec4f(position, 0.5, 1.0);
-    out.color = vec4f(in.color, 1 / (1 + exp(-in.opacity)));
-    out.uv = in.quad_pos / 2.0 + 0.5;
-    return out;
-}`;
-
 const splatFragmentWgsl = `
 @group(0) @binding(1) var linearSampler : sampler;
 @group(0) @binding(2) var gaussianTexture : texture_2d<f32>;
@@ -172,6 +138,7 @@ fn fs_main(
     return textureSample(gaussianTexture, linearSampler, fragUV) * color;
 }
 `;
+
 async function getDevice() {
   const adapter = await navigator.gpu.requestAdapter();
   const device = await adapter.requestDevice();
@@ -233,15 +200,11 @@ async function main() {
   const splatSize = 17;
   const splatSizeByte = splatSize * 4;
 
-
-  
-
-
   const pipeline = device.createRenderPipeline({
     layout: "auto",
     vertex: {
       module: device.createShaderModule({
-        code: splatVertexWgsl,
+        code: await (await fetch('vertex.wgsl')).text(),
       }),
       buffers: [
         {
---
+++ webgpu/vertex.wgsl
@@ -0,0 +1,27 @@
+struct VertexInput {
+    @builtin(instance_index) instance_index: u32,
+    @location(0) position: vec2f,
+    @location(1) color: vec3f,
+    @location(2) opacity: f32,
+    @location(3) quad_pos: vec2f,
+    @location(4) eigen: vec4f,
+}
+
+struct VertexOutput {
+    @builtin(position) position : vec4f,
+    @location(0) color : vec4f,
+    @location(1) uv: vec2f,
+}
+
+@vertex
+fn vs_main(in : VertexInput) -> VertexOutput {
+    // var position = vec3f(in.position + 0.1 * in.quad_pos, 1.0);
+    var eigen_mat = 1 * mat2x2f(in.eigen.xy, in.eigen.zw);
+    var position = in.position + eigen_mat * in.quad_pos;
+
+    var out : VertexOutput;
+    out.position = vec4f(position, 0.5, 1.0);
+    out.color = vec4f(in.color, .05 * 1 / (1 + exp(-in.opacity)));
+    out.uv = in.quad_pos / 2.0 + 0.5;
+    return out;
+}
